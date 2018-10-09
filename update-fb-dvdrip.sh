ca94d9a679f72f0b86b96053fe799a1099dbf737
limelime <limelime@users.noreply.github.com>
2018-10-09 20:55:05 +0000

    fix firstboot path.

--- update-fb-dvdrip.sh
+++ update-fb-dvdrip.sh
@@ -2,7 +2,7 @@
 set -e
 # Description: Update Firstboot vbox script.
 
-fb_run_dir=$(readlink -ev /media/master/github/firstboot/apps/dvdrip/run)
+fb_run_dir=$(readlink -ev /media/master/github/firstboot/firstboot/apps/dvdrip/run)
 dvdrip_script_dir=$(readlink -ev ./dvdrip)
 
 # Update dvdrip to firstboot.
